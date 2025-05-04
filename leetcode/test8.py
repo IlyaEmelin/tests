a8f13211fad7a6cba588a17ebd417202903f48cb
Илья <bkmz8_133@mail.ru>
2025-05-04 08:25:19 +0300

    optimize

--- leetcode/test8.py
+++ leetcode/test8.py
@@ -1,35 +1,60 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        is_positive, num = True, 0
-        s_it = iter(s)
-        for char in s_it:
+        offset = len(s)
+        for i in range(len(s)):
             # 1. Whitespace
-            if char == ' ':
-                continue
-            # 2. Signedness
-            elif char == '-':
+            if s[i] != ' ':
+                offset = i
+                break
+
+        # 2. Signedness
+        is_positive = True
+        if offset < len(s):
+            if s[offset] == '+':
+                offset += 1
+            elif s[offset] == '-':
+                offset += 1
                 is_positive = False
-            elif char == '+':
-                is_positive = True
-            # 3. Conversion
-            elif char in '0123456789':
-                num = int(char)
+
+        # 3. Conversion
+        start_num, end_num = -1, -1
+        for i in range(offset, len(s)):
+            if s[i] in '0123456789':
+                start_num, end_num = i, i+1
+            else:
+                return 0
+            offset += 1
             break
 
         # 3.Conversion
-        max_int, min_int = 2**31 - 1, 2**31
-        for char in s_it:
-            if char in '0123456789':
-                num = num * 10 + int(char)
-                # 4. Rounding
-                if is_positive and num > max_int:
-                    return max_int
-                elif not is_positive and num > min_int:
-                    return -min_int
+        for i in range(offset, len(s)):
+            if s[i] in '0123456789':
+                end_num = i+1
             else:
                 break
-        return num if is_positive else -num
+
+        # 4. Rounding
+        if start_num >=0:
+            value = int(s[start_num:end_num])
+            if is_positive:
+                if (max_int := 2**31 - 1) < value:
+                    return max_int
+                return value
+            else:
+                if (min_int := 2**31) < value:
+                    return -min_int
+                return -value
+        return 0
 
 
 if __name__ == "__main__":
-    print(Solution().myAtoi(".1"))
+    print(".1", Solution().myAtoi(".1"), 0, sep='|')
+    print("42", Solution().myAtoi("42"), 42, sep='|')
+    print("   -042", Solution().myAtoi("   -042"), -42, sep='|')
+    print("0 - 1", Solution().myAtoi("0 - 1"), 0, sep='|')
+    print("1337c0d3", Solution().myAtoi("1337c0d3"), 1337, sep='|')
+    print("0-1", Solution().myAtoi("0-1"), 0, sep='|')
+    print("words and 987", Solution().myAtoi("words and 987"), 0, sep='|')
+    print("-91283472332", Solution().myAtoi("-91283472332"), -2147483648, sep='|')
+    print("", Solution().myAtoi(""), 0, sep='|')
+    print(" ", Solution().myAtoi(" "), 0, sep='|')
