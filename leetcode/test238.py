e310574846969ccac2edfebf73b7a55722c2ecbd
Илья <bkmz8_133@mail.ru>
2025-06-21 16:29:56 +0300

    optimize

--- leetcode/test238.py
+++ leetcode/test238.py
@@ -1,20 +1,33 @@
 from typing import List
 
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         left, right = [1]*(len(nums)), [1]*(len(nums))
+#         last = 1
+#         for i, num in enumerate(nums):
+#             left[i] = last
+#             last = num * last
+#
+#         last = 1
+#         for i, num in enumerate(reversed(nums)):
+#             right[len(nums)- 1 - i] = last
+#             last = num * last
+#
+#         return [ num1 * num2 for num1, num2 in zip(left, right) ]
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left, right = [1]*(len(nums)), [1]*(len(nums))
+        result = [1] * len(nums)
         last = 1
         for i, num in enumerate(nums):
-            left[i] = last
-            last = num * last
+            result[i] = last
+            last *= num
 
         last = 1
-        for i, num in enumerate(reversed(nums)):
-            right[len(nums)- 1 - i] = last
-            last = num * last
-
-        return [ num1 * num2 for num1, num2 in zip(left, right) ]
-
+        for index in range(len(nums)-1, -1, -1):
+            result[index] *= last
+            last *= nums[index]
+        return result
 
 if __name__ == "__main__":
     print(Solution().productExceptSelf([1, 2, 3 , 4]))
